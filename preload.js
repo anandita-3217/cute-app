d5d8d2b0b996bc71f63d865e9f2d518b95f5c529
Anandita <dakshugarimella@gmail.com>
2025-03-28 18:18:08 +0530

    modified code

--- preload.js
+++ preload.js
@@ -1,7 +1,8 @@
-const {contextBridge,ipcRenderer } = require("electron");
-contextBridge.executeInMainWorld("electronAPI",{
-    loadPage: (page) => ipcRenderer.send("load-page",page),
-    minimizeWindow: () => ipcRenderer.send("minimize-window"),
-    minimizeWindow: () => ipcRenderer.send("maximize-window"),
-    closeWindow: () => ipcRenderer.send("close-window"),
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  loadPage: (page) => ipcRenderer.send("load-page", page),
+  minimizeWindow: () => ipcRenderer.send("minimize-window"),
+  maximizeWindow: () => ipcRenderer.send("maximize-window"),
+  closeWindow: () => ipcRenderer.send("close-window")
 });
