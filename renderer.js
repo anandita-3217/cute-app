70d1fb848689b8f99053cc7e9076436b7d931811
Anandita <dakshugarimella@gmail.com>
2025-03-28 21:16:30 +0530

    Works.. kinda check for the easteregg functionality thi

--- renderer.js
+++ renderer.js
@@ -10,12 +10,27 @@ document.getElementById('maximize-btn').addEventListener('click', () => {
   });
 
   document.getElementById('action1').addEventListener('click', () => {
-    alert('You did something fun!');
-  });
+    // Enhanced fun action with color change
+    const mascot = document.querySelector('.mascot img');
+    mascot.classList.add('rainbow-effect');
+    setTimeout(() => {
+        mascot.classList.remove('rainbow-effect');
+    }, 20000);
+});
+
   
   document.getElementById('action2').addEventListener('click', () => {
     document.querySelector('.mascot img').classList.add('spin');
     setTimeout(() => {
       document.querySelector('.mascot img').classList.remove('spin');
     }, 1000);
+  });
+  
+  let secretClickCount = 0;
+  document.querySelector.apply('.mascot img').addEventListener('click',() =>{
+    secretClickCount++;
+    if(secretClickCount >= 5){
+      alert('You found the secret! ✨');
+      secretClickCount = 0;
+    }
   });
